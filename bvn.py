028f57cebcb277e2d42945a8fce4138c08f1b70d
Chidera Divine-gift <chrischidera6@gmail.com>
2024-04-16 11:20:27 +0100

    update commit

---
+++ bvn.py
@@ -0,0 +1,67 @@
+class BVN:
+    bvn = None
+    first_name = None
+    middle_name = None
+    last_name = None
+    address = None
+    email = None
+    phone_number = None
+    dob = None
+    nationality = None
+    created_date = None
+    last_updated = None
+    status_code = None
+
+    @classmethod
+    def register_bvn(cls):
+        """Method to register a new Bank Verification Number for a user, capturing their personal information and
+        biometric data."""
+        pass
+
+    @classmethod
+    def verify_bvn(cls):
+        """Method to verify the authenticity of a BVN, validating it against the central database or authority to
+        ensure it is valid and active."""
+        pass
+
+    @classmethod
+    def update_bvn(cls):
+        """Method to update the information associated with a BVN, such as personal details or biometric data,
+        ensuring the data remains accurate and up to date."""
+        pass
+
+    @classmethod
+    def link_bvn_to_account(cls):
+        """Method to link a BVN to a user's bank account, enabling seamless identification and verification during
+        account-related transactions."""
+        pass
+
+    @classmethod
+    def get_bvn_verification(cls):
+        """Method to retrieve information associated with a BVN, including personal details, linked accounts,
+        and verification status."""
+        pass
+
+    @classmethod
+    def deactivation(cls):
+        """Method to deactivate or suspend a BVN, typically in cases of fraud, identity theft, or other security
+        concerns."""
+        pass
+
+    @classmethod
+    def reactivation(cls):
+        """Method to re-activate a previously deactivated BVN, once any issues or concerns have been resolved and the
+        BVN is deemed valid again."""
+        pass
+
+    @classmethod
+    def delete_bvn(cls):
+        """Method to permanently delete a BVN record from the system, typically in cases where the BVN is no longer
+        required or has been replaced."""
+        pass
+
+    @classmethod
+    def authenticate_bvn(cls):
+        """Method to authenticate the identity of a user based on their BVN, verifying their identity during
+        account-related transactions or authentication processes."""
+        pass

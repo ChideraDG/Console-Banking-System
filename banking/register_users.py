0f9345394a2073c414e63b6dfff370638fe2ed03
Chidera Divine-gift <chrischidera6@gmail.com>
2024-04-26 15:20:06 +0100

    update

--- banking/register_users.py
+++ banking/register_users.py
@@ -49,6 +49,257 @@ def countdown_timer(register):
         print(end='\r')
 
 
+def first_name() -> str:
+    while True:
+        print("\nInput your First Name:")
+        print("~~~~~~~~~~~~~~~~~~~~~~")
+        name = input(">>> ").strip().title()
+
+        if '-' in name:
+            if name[:name.index('-')].isalpha() and name[name.index('-'):].isalpha():
+                break
+            break
+        elif name.isalpha():
+            break
+        else:
+            print('\n*ERROR*')
+            print("-> Names should be in letters only.\nExample: James, Mary, etc.")
+            time.sleep(3)
+            continue
+
+    return name
+
+
+def middle_name() -> str:
+    while True:
+        print("\nInput your Middle Name:")
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        name = input(">>> ").strip().title()
+
+        if '-' in name:
+            if name[:name.index('-')].isalpha() and name[name.index('-'):].isalpha():
+                break
+            break
+        elif name.isalpha():
+            break
+        else:
+            print('\n*ERROR*')
+            print("-> Names should be in letters only.\nExample: James, Mary, etc.")
+            time.sleep(3)
+            continue
+
+    return name
+
+
+def last_name() -> str:
+    while True:
+        print("\nInput your Last Name:")
+        print("~~~~~~~~~~~~~~~~~~~~~")
+        name = input(">>> ").strip().title()
+
+        if '-' in name:
+            if name[:name.index('-')].isalpha() and name[name.index('-'):].isalpha():
+                break
+            break
+        elif name.isalpha():
+            break
+        else:
+            print('\n*ERROR*')
+            print("-> Names should be in letters only.\nExample: James, Mary, etc.")
+            time.sleep(3)
+            continue
+
+    return name
+
+
+def gender() -> str:
+    while True:
+        print("\nInput your Gender:")
+        print("~~~~~~~~~~~~~~~~~~~~~")
+        _gender = input(">>> ").strip().title()
+
+        if not _gender.isalpha():
+            print('\n*ERROR*')
+            print("-> Gender should be in letters only.\nExample: Male, Female")
+            time.sleep(3)
+            continue
+        elif not (_gender == 'Male' or _gender == 'Female'):
+            print('\n*ERROR*')
+            print("-> Gender should be either Male or Female only.")
+            time.sleep(3)
+            continue
+        else:
+            break
+
+    return _gender
+
+
+def address() -> str:
+    print("\nInput your Address:")
+    print("~~~~~~~~~~~~~~~~~~~")
+    _address = input(">>> ").strip().title()
+
+    return _address
+
+
+def date_of_birth() -> str:
+    present_year = dt.datetime.today().year
+    max_year = 2006
+    month = 12
+    month_name = 'December'
+    days = 31
+
+    while True:
+        print("\nInput your Year of Birth:")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+        year_of_birth = input(">>> ").strip()
+
+        if year_of_birth.isdigit() and 1900 < int(year_of_birth) <= max_year:
+            break
+        else:
+            print('\n*ERROR*')
+            if year_of_birth.isdigit():
+                if not int(year_of_birth) <= max_year:
+                    print("-> Age is less than 18")
+                if not int(year_of_birth) >= present_year:
+                    print(f"-> Year {year_of_birth} not in existence yet")
+                elif not 1900 < int(year_of_birth):
+                    print("-> Year is less than 1900")
+            else:
+                print("-> Year of Birth should be in digits.\nExample: 2001, 2004, etc.")
+            time.sleep(3)
+            continue
+
+    time.sleep(1)
+
+    while True:
+        print("\nInput your Month of Birth:")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        month_of_birth = input(">>> ").strip()
+
+        if month_of_birth.isdigit() and 0 < int(month_of_birth) <= month:
+            break
+        else:
+            print('\n*ERROR*')
+            if month_of_birth.isdigit():
+                if not 0 < int(month_of_birth) <= 12:
+                    print("-> Month of Birth should be between Zero(0) and Twelve(12)")
+            else:
+                print("-> Month of Birth should be in digits.\nExample: 2 means February, 4 means April, etc.")
+            time.sleep(3)
+            continue
+
+    month = int(month_of_birth)
+    if month == 1:
+        month_name = 'January'
+        days = 31
+    elif month == 2:
+        month_name = 'February'
+        if (max_year % 4 == 0 and max_year % 100 != 0) or (max_year % 400 == 0):
+            days = 29
+        else:
+            days = 28
+    elif month == 3:
+        month_name = 'March'
+        days = 31
+    elif month == 4:
+        month_name = 'April'
+        days = 30
+    elif month == 5:
+        month_name = 'May'
+        days = 31
+    elif month == 6:
+        month_name = 'June'
+        days = 30
+    elif month == 7:
+        month_name = 'July'
+        days = 31
+    elif month == 8:
+        month_name = 'August'
+        days = 30
+    elif month == 9:
+        month_name = 'September'
+        days = 30
+    elif month == 10:
+        month_name = 'October'
+        days = 31
+    elif month == 11:
+        month_name = 'November'
+        days = 30
+    elif month == 12:
+        month_name = 'December'
+        days = 31
+
+    time.sleep(1)
+
+    while True:
+        print("\nInput your Day of Birth:")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~")
+        day_of_birth = input(">>> ").strip()
+
+        if day_of_birth.isdigit() and 0 < int(day_of_birth) <= days:
+            break
+        else:
+            print('\n*ERROR*')
+            if day_of_birth.isdigit():
+                if not 0 < int(day_of_birth) <= days:
+                    print(f"-> Day of Birth should be within the number of days in {month_name}")
+            else:
+                print("-> Day of Birth should be in digits.\nExample: 2, 4, 10, etc.")
+            time.sleep(3)
+            continue
+
+    return f'{year_of_birth}-{month_of_birth}-{day_of_birth}'
+
+
+def e_mail() -> str:
+    while True:
+        print("\nInput your E-mail:")
+        print("~~~~~~~~~~~~~~~~~~")
+        email = input(">>> ").strip().lower()
+
+        if get_data('email', email):
+            print('\n*ERROR*')
+            print("-> Email already exist")
+            time.sleep(3)
+            continue
+
+        if email[-4:] == '.com' and '@' in email:
+            break
+        else:
+            print('\n*ERROR*')
+            print("-> Invalid Email.\nExample: himates@gamil.com, markjames@yahoo.com etc.")
+            time.sleep(3)
+            continue
+
+    return email
+
+
+def phone_number() -> str:
+    while True:
+        print("\nInput your Phone Number:")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~")
+        phoneNumber = input(">>> ").strip()
+
+        if get_data('phone_number', phoneNumber):
+            print('\n*ERROR*')
+            print("-> Phone Number already exist")
+            time.sleep(3)
+            continue
+
+        if phoneNumber.isdigit() and 11 <= len(phoneNumber) <= 15:
+            break
+        elif phoneNumber[1:].isdigit() and phoneNumber[0] == '+':
+            break
+        else:
+            print('\n*ERROR*')
+            print("-> Phone Number should be in digits  only.\nExample: 08076542879, +2348033327493 etc.")
+            time.sleep(3)
+            continue
+
+    return phoneNumber
+
+
 def register_bvn():
     """Registration Form"""
 
@@ -59,245 +310,45 @@ def register_bvn():
         print("\nInstruction: Carefully fill in your details")
         print("==========================================\n")
 
-        time.sleep(1)
+        time.sleep(2)
 
-        while True:
-            print("\nInput your First Name:")
-            print("~~~~~~~~~~~~~~~~~~~~~~")
-            first_name = input(">>> ").strip().title()
-
-            if '-' in first_name:
-                if first_name[:first_name.index('-')].isalpha() and first_name[first_name.index('-'):].isalpha():
-                    break
-                break
-            elif first_name.isalpha():
-                break
-            else:
-                print('\n*ERROR*')
-                print("-> Names should be in letters only.\nExample: James, Mary, etc.")
-                time.sleep(3)
-                continue
+        _firstname = first_name()
 
         time.sleep(1)
 
-        while True:
-            print("\nInput your Middle Name:")
-            print("~~~~~~~~~~~~~~~~~~~~~~~")
-            middle_name = input(">>> ").strip().title()
-
-            if '-' in middle_name:
-                if middle_name[:middle_name.index('-')].isalpha() and middle_name[middle_name.index('-'):].isalpha():
-                    break
-                break
-            elif middle_name.isalpha():
-                break
-            else:
-                print('\n*ERROR*')
-                print("-> Names should be in letters only.\nExample: James, Mary, etc.")
-                time.sleep(3)
-                continue
+        _middlename = middle_name()
 
         time.sleep(1)
 
-        while True:
-            print("\nInput your Last Name:")
-            print("~~~~~~~~~~~~~~~~~~~~~")
-            last_name = input(">>> ").strip().title()
-
-            if '-' in last_name:
-                if last_name[:last_name.index('-')].isalpha() and last_name[last_name.index('-'):].isalpha():
-                    break
-                break
-            elif last_name.isalpha():
-                break
-            else:
-                print('\n*ERROR*')
-                print("-> Names should be in letters only.\nExample: James, Mary, etc.")
-                time.sleep(3)
-                continue
+        _lastname = last_name()
 
         time.sleep(1)
 
-        while True:
-            print("\nInput your Gender:")
-            print("~~~~~~~~~~~~~~~~~~~~~")
-            gender = input(">>> ").strip().title()
-
-            if not gender.isalpha():
-                print('\n*ERROR*')
-                print("-> Gender should be in letters only.\nExample: Male, Female")
-                time.sleep(3)
-                continue
-            elif not (gender == 'Male' or gender == 'Female'):
-                print('\n*ERROR*')
-                print("-> Gender should be either Male or Female only.")
-                time.sleep(3)
-                continue
-            else:
-                break
+        _gender = gender()
 
         time.sleep(1)
 
-        print("\nInput your Address:")
-        print("~~~~~~~~~~~~~~~~~~~")
-        address = input(">>> ").strip().title()
-
-        max_year = 2006
-        month = 12
-        month_name = 'December'
-        days = 31
+        _address = address()
 
         time.sleep(1)
 
-        while True:
-            print("\nInput your Year of Birth:")
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-            year_of_birth = input(">>> ").strip()
-
-            if year_of_birth.isdigit() and 1900 < int(year_of_birth) <= max_year:
-                break
-            else:
-                print('\n*ERROR*')
-                if year_of_birth.isdigit():
-                    if not int(year_of_birth) <= max_year:
-                        print("-> Age is less than 18")
-                    elif not 1900 < int(year_of_birth):
-                        print("-> Year is less than 1900")
-                else:
-                    print("-> Year of Birth should be in digits.\nExample: 2001, 2004, etc.")
-                time.sleep(3)
-                continue
+        _dob = date_of_birth()
 
         time.sleep(1)
 
-        while True:
-            print("\nInput your Month of Birth:")
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            month_of_birth = input(">>> ").strip()
-
-            if month_of_birth.isdigit() and 0 < int(month_of_birth) <= month:
-                break
-            else:
-                print('\n*ERROR*')
-                if month_of_birth.isdigit():
-                    if not 0 < int(month_of_birth) <= 12:
-                        print("-> Month of Birth should be between Zero(0) and Twelve(12)")
-                else:
-                    print("-> Month of Birth should be in digits.\nExample: 2 means February, 4 means April, etc.")
-                time.sleep(3)
-                continue
-
-        month = int(month_of_birth)
-        if month == 1:
-            month_name = 'January'
-            days = 31
-        elif month == 2:
-            month_name = 'February'
-            if (max_year % 4 == 0 and max_year % 100 != 0) or (max_year % 400 == 0):
-                days = 29
-            else:
-                days = 28
-        elif month == 3:
-            month_name = 'March'
-            days = 31
-        elif month == 4:
-            month_name = 'April'
-            days = 30
-        elif month == 5:
-            month_name = 'May'
-            days = 31
-        elif month == 6:
-            month_name = 'June'
-            days = 30
-        elif month == 7:
-            month_name = 'July'
-            days = 31
-        elif month == 8:
-            month_name = 'August'
-            days = 30
-        elif month == 9:
-            month_name = 'September'
-            days = 30
-        elif month == 10:
-            month_name = 'October'
-            days = 31
-        elif month == 11:
-            month_name = 'November'
-            days = 30
-        elif month == 12:
-            month_name = 'December'
-            days = 31
+        _email = e_mail()
 
         time.sleep(1)
 
-        while True:
-            print("\nInput your Day of Birth:")
-            print("~~~~~~~~~~~~~~~~~~~~~~~~")
-            day_of_birth = input(">>> ").strip()
-
-            if day_of_birth.isdigit() and 0 < int(day_of_birth) <= days:
-                break
-            else:
-                print('\n*ERROR*')
-                if day_of_birth.isdigit():
-                    if not 0 < int(day_of_birth) <= days:
-                        print(f"-> Day of Birth should be within the number of days in {month_name}")
-                else:
-                    print("-> Day of Birth should be in digits.\nExample: 2, 4, 10, etc.")
-                time.sleep(3)
-                continue
-
-        time.sleep(1)
-
-        while True:
-            print("\nInput your E-mail:")
-            print("~~~~~~~~~~~~~~~~~~")
-            email = input(">>> ").strip().lower()
-
-            if get_data('email', email):
-                print('\n*ERROR*')
-                print("-> Email already exist")
-                time.sleep(3)
-                continue
-
-            if email[-4:] == '.com' and '@' in email:
-                break
-            else:
-                print('\n*ERROR*')
-                print("-> Invalid Email.\nExample: himates@gamil.com, markjames@yahoo.com etc.")
-                time.sleep(3)
-                continue
-
-        time.sleep(1)
-
-        while True:
-            print("\nInput your Phone Number:")
-            print("~~~~~~~~~~~~~~~~~~~~~~~~")
-            phone_number = input(">>> ").strip()
-
-            if get_data('phone_number', phone_number):
-                print('\n*ERROR*')
-                print("-> Phone Number already exist")
-                time.sleep(3)
-                continue
-
-            if phone_number.isdigit() and 11 <= len(phone_number) <= 15:
-                break
-            elif phone_number[1:].isdigit() and phone_number[0] == '+':
-                break
-            else:
-                print('\n*ERROR*')
-                print("-> Phone Number should be in digits  only.\nExample: 08076542879, +2348033327493 etc.")
-                time.sleep(3)
-                continue
+        _phoneNumber = phone_number()
 
         created_bvn = str(random.randint(100000000000, 999999999999))
         while get_data('bvn_number', created_bvn):
             created_bvn = str(random.randint(100000000000, 999999999999))
 
-        register = bvn.BVN(first_name=first_name.title(), middle_name=middle_name.title(), last_name=last_name.title(),
-                           gender=gender, address=address.title(), email=email.lower(), phone_number=phone_number,
-                           created_date=dt.datetime.now(), date_of_birth=f'{year_of_birth}-{month_of_birth}-{day_of_birth}',
+        register = bvn.BVN(first_name=_firstname.title(), middle_name=_middlename.title(), last_name=_lastname.title(),
+                           gender=_gender.title(), address=_address.title(), email=_email.lower(),
+                           phone_number=_phoneNumber, created_date=dt.datetime.now(), date_of_birth=_dob,
                            bvn_status='active', bvn_number=created_bvn, last_updated=dt.datetime.now())
 
         register.register_bvn()
@@ -311,4 +362,4 @@ def register_bvn():
         print(f"\nUser: {register.last_name} {register.first_name} {register.middle_name}")
         print(f"BVN NUMBER: {register.bvn_number}")
     except Exception:
-        print(f"Error registering BVN")
+        print(f"\n*ERROR*\nError registering BVN")

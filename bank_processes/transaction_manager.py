1f08f9930f36753b8ab7e480037122d255121011
akhereoboh <victoryoboh10@gmail.com>
2024-04-27 11:29:16 +0100

    Update transaction_manager.py

--- bank_processes/transaction_manager.py
+++ bank_processes/transaction_manager.py
@@ -2,12 +2,11 @@ from bank_processes.database import DataBase
 
 
 class TransactionManager:
-    def __init__(self, trans_queue, trans_history, trans_limit_config, trans_fees,
-                 trans_statues, error_log, trans_processing_policies, trans_processing_time, trans_monitoring_settings):
+    def __init__(self, trans_queue: str = None, trans_history: str = None, trans_limit_config: str = None,
+                 trans_statues: str = None, error_log: str = None, trans_processing_policies: str = None, trans_processing_time: str = None, trans_monitoring_settings: str = None ):
         self.trans_queue = trans_queue  # A queue data structure to store pending transactions awaiting processing.
         self.trans_history = trans_history  # A data structure to store completed transactions.
         self.trans_limit_config = trans_limit_config  # Configuration settings for transaction limits.
-        self.trans_fees = trans_fees  # Information about fees associated with different types of transactions.
         self.trans_statues = trans_statues  # Enumeration of possible transaction statuses.
         self.error_log = error_log  # A log to record errors and exceptions encountered during transaction processing.
         self.trans_processing_policies = trans_processing_policies  # For dictating the rules & procedures for processing transactions.

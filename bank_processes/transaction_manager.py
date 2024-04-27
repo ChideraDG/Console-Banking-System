ef27b2281a446ca39bd12a809bb3799a89dfb3bd
akhereoboh <victoryoboh10@gmail.com>
2024-04-27 11:33:05 +0100

    Update transaction_manager.py

--- bank_processes/transaction_manager.py
+++ bank_processes/transaction_manager.py
@@ -1,9 +1,12 @@
 from bank_processes.database import DataBase
+from bank_processes.transaction import Transaction
 
 
-class TransactionManager:
+class TransactionManager(Transaction):
     def __init__(self, trans_queue: str = None, trans_history: str = None, trans_limit_config: str = None,
-                 trans_statues: str = None, error_log: str = None, trans_processing_policies: str = None, trans_processing_time: str = None, trans_monitoring_settings: str = None ):
+                 trans_statues: str = None, error_log: str = None, trans_processing_policies: str = None,
+                 trans_processing_time: str = None, trans_monitoring_settings: str = None):
+        super().__init__()
         self.trans_queue = trans_queue  # A queue data structure to store pending transactions awaiting processing.
         self.trans_history = trans_history  # A data structure to store completed transactions.
         self.trans_limit_config = trans_limit_config  # Configuration settings for transaction limits.
@@ -16,6 +19,7 @@ class TransactionManager:
 
     def transfer_funds(self):
         """Method to facilitate transferring funds between accounts"""
+
         pass
 
     def deposit_funds(self):
@@ -24,10 +28,6 @@ class TransactionManager:
 
     def withdraw_funds(self):
         """Method to enable users to withdraw money from their accounts"""
-        pass
-
-    def handle_transfers_between_banks(self):
-        """Method to handle transfers between accounts held at different banks"""
         pass
 
     def validate_transactions(self):

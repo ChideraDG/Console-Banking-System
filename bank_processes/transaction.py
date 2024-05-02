0be3192cf4cb5c07648d8b367a67e2491c69fa15
Dozieking <chiedoziee406@gmail.com>
2024-05-02 12:25:48 -0700

    update

--- bank_processes/transaction.py
+++ bank_processes/transaction.py
@@ -13,63 +13,64 @@ class Transaction:
                  receiver_acct_num: str = None,
                  description: str = None, status: str = None, fees: float = None, merchant_info: str = None,
                  transaction_category: str = None, user_id: str = None, account_type: str = None,
-                 sender_name: str = None, receiver_name: str = None):
-        self.transaction_type = transaction_type
-        self.amount = amount
-        self.transaction_id = transaction_id  # unique identifier for transaction
-        self.transaction_date_time = transaction_date_time  # timestamp for when the transaction occurred
-        self.sender_acct_num = sender_acct_num  # sender's account number
-        self.receiver_acct_num = receiver_acct_num  # receiver's account number
-        self.description = description  # description of the transaction
-        self.status = status  # status of the transaction
-        self.fees = fees  # fees associated with the amount
-        self.merchant_info = merchant_info  # info about the merchant or receiver
-        self.transaction_category = transaction_category  # category of the transfer
-        self.user_id = user_id  # identifier of the user who initiated the transaction
-        self.account_type = account_type  # whether fixed deposit, savings or current
-        self.sender_name = sender_name  # Person in question doing the transaction
-        self.receiver_name = receiver_name
+                 sender_name: str = None, receiver_name: str = None, balance: float = None):
+        self.__transaction_type = transaction_type
+        self.__amount = amount
+        self.__transaction_id = transaction_id  # unique identifier for transaction
+        self.__transaction_date_time = transaction_date_time  # timestamp for when the transaction occurred
+        self.__sender_acct_num = sender_acct_num  # sender's account number
+        self.__receiver_acct_num = receiver_acct_num  # receiver's account number
+        self.__description = description  # description of the transaction
+        self.__status = status  # status of the transaction
+        self.__fees = fees  # fees associated with the amount
+        self.__merchant_info = merchant_info  # info about the merchant or receiver
+        self.__transaction_category = transaction_category  # category of the transfer
+        self.__user_id = user_id  # identifier of the user who initiated the transaction
+        self.__account_type = account_type  # whether fixed deposit, savings or current
+        self.__sender_name = sender_name  # Person in question doing the transaction
+        self.__receiver_name = receiver_name
+        self.__balance = balance
 
     def transaction_record(self):
         """Method to record new transactions made and the relevant information"""
         now = datetime.datetime.now()
-        self.transaction_date_time = now.strftime("%d %B %Y %H:%M:%S")
+        self.__transaction_date_time = now.strftime("%d %B %Y %H:%M:%S")
         amt = ''
         detail_type = ''  # whether sender or receiver
         option_transfer = ''  # 'Payment method' in the  case of a debit and 'Credited' for credit
-        self.transaction_type = self.transaction_type.upper()
-        if self.transaction_type == 'WITHDRAW':
-            amt = f'-{self.amount}'
-            detail_type = f'''Recipient Details\t\t\t{self.sender_name}
-                          \t\t\tBankApp|{self.sender_acct_num}'''
+        self.__transaction_type = self.__transaction_type.upper()
+        if self.__transaction_type == 'WITHDRAW':
+            amt = f'-{self.__amount}'
+            detail_type = f'''Recipient Details\t\t\t{self.__sender_name}
+                          \t\t\tBankApp|{self.__sender_acct_num}'''
             option_transfer = f'''Credited to         \t\tBalance'''
 
-        elif self.transaction_type == 'TRANSFER':
-            amt = f'-{self.amount}'
-            detail_type = f'''Recipient Details\t\t\t{self.sender_name}
-                          \t\t\tBankApp|{self.sender_acct_num}'''
+        elif self.__transaction_type == 'TRANSFER':
+            amt = f'-{self.__amount}'
+            detail_type = f'''Recipient Details\t\t\t{self.__sender_name}
+                          \t\t\tBankApp|{self.__sender_acct_num}'''
             option_transfer = f'''Credited to         \t\tBalance'''
 
-        elif self.transaction_type == 'DEPOSIT':
-            amt = f'+{self.amount}'
-            detail_type = f'''Sender Details\t\t\t\t{self.sender_name}
-                          \t\t\tBankApp|{self.sender_acct_num}'''
+        elif self.__transaction_type == 'DEPOSIT':
+            amt = f'+{self.__amount}'
+            detail_type = f'''Sender Details\t\t\t\t{self.__sender_name}
+                          \t\t\tBankApp|{self.__sender_acct_num}'''
             option_transfer = f'''Payment Method         \t\tBalance'''
 
-            while verify_data('transaction_id', self.transaction_id):
-                self.transaction_id = {random.randint(100000000000000000000000000000,
-                                                      999999999999999999999999999999)}
+            while verify_data('transaction_id', self.__transaction_id):
+                self.__transaction_id = {random.randint(100000000000000000000000000000,
+                                                        999999999999999999999999999999)}
 
         trans_record = f"""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                     {amt}
-                    {self.status}
+                    {self.__status}
         TRANSACTION DETAILS
-        Transaction Type\t\t\t{self.transaction_type}
+        Transaction Type\t\t\t{self.__transaction_type}
         {detail_type}
-        Description        \t\t\t{self.description}
+        Description        \t\t\t{self.__description}
         {option_transfer}
-        Transaction Date      \t\t{self.transaction_date_time}
-        Transaction ID         \t\t{self.transaction_id}
+        Transaction Date      \t\t{self.__transaction_date_time}
+        Transaction ID         \t\t{self.__transaction_id}
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
         print(trans_record)
 
@@ -79,12 +80,14 @@ class Transaction:
 
     def cal_transaction_fees(self):
         """Method to calculate fees associated with the transfer depending on the amount """
-        return self.fees + self.amount
+        return self.__fees + self.__amount
 
     def transaction_validation(self):
         """Method to validate the transaction, ensuring that it meets any requirements or constraints imposed by
         the bank or regulatory authorities. """
-        pass
+        if self.__balance + self.__fees > self.__balance:
+            return True
+        el
 
     def process_transaction(self):
         """Method to process the transaction, including updating account balances, recording transaction details,
@@ -109,7 +112,6 @@ class Transaction:
     def transaction_history(self):
         """ Method to retrieve the transaction history associated with a specific account or user,
          providing details of past transactions for reference and auditing purposes."""
-
 
 # obj = Transaction(transaction_type='withdraw', sender_name='Ezenwa Chiedozie', description='Food',
 #                   sender_acct_num='12132537437', status='SUCCESS', amount=500.00)
